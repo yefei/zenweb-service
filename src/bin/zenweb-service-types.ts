9ba382683a9bdbc5318a665e98323a199928d651
YeFei <316606233@qq.com>
2021-12-23 15:15:51 +0800

    service 文件夹默认找 ./src/service

--- src/bin/zenweb-service-types.ts
+++ src/bin/zenweb-service-types.ts
@@ -6,7 +6,7 @@ const pathEnv = process.env.ZENWEB_SERVICE_PATHS;
 const typingEnv = process.env.ZENWEB_SERVICE_TYPING_FILE;
 const patternsEnv = process.env.ZENWEB_SERVICE_PATTERNS;
 
-const paths = pathEnv ? pathEnv.split(':') : [path.join(process.cwd(), 'app', 'service')];
+const paths = pathEnv ? pathEnv.split(':') : [path.join(process.cwd(), 'src', 'service')];
 const typingFile = typingEnv || path.resolve(process.cwd(), 'typings', 'service.ts');
 
 findServicesToTyping(paths, typingFile, patternsEnv).then(() => {

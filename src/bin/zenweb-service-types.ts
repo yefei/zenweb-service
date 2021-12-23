56749c0086e51472c35641eb28929bb867d09cda
YeFei <316606233@qq.com>
2021-12-23 15:31:18 +0800

    _typings

--- src/bin/zenweb-service-types.ts
+++ src/bin/zenweb-service-types.ts
@@ -7,7 +7,7 @@ const typingEnv = process.env.ZENWEB_SERVICE_TYPING_FILE;
 const patternsEnv = process.env.ZENWEB_SERVICE_PATTERNS;
 
 const paths = pathEnv ? pathEnv.split(':') : [path.join(process.cwd(), 'src', 'service')];
-const typingFile = typingEnv || path.join(process.cwd(), 'src', 'service', '__typings.ts');
+const typingFile = typingEnv || path.join(process.cwd(), 'src', '_typings', 'service.ts');
 
 findServicesToTyping(paths, typingFile, patternsEnv).then(() => {
   console.log('make service typing success');
